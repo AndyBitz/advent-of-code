7f79bd61498d9671ef30099778f7e507ecc3d873
Andy Bitz <artzbitz@gmail.com>
2022-12-05 21:13:44 +0100

    Add 2022-12-05

---
+++ 2022/day-05/src/main.rs
@@ -0,0 +1,101 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = io::BufReader::new(file).lines();
+
+    let mut stacks_part_1: Vec<Vec<char>> = vec![Vec::new(); 9];
+    let mut stacks_part_2 = stacks_part_1.clone();
+
+    let mut do_movement = false;
+
+    for (_, line) in lines.enumerate() {
+        let line = line.unwrap();
+
+        if line.starts_with(" 1") {
+            continue;
+        }
+
+        if line == "" {
+            println!("Initial state:");
+            for i in 0..stacks_part_1.len() {
+                println!("{}: {:?}", i, stacks_part_1[i]);
+            }
+            println!("");
+
+            stacks_part_2 = stacks_part_1.clone();
+            do_movement = true;
+
+            continue;
+        }
+
+        if do_movement == false {
+            let chars: Vec<char> = line.chars().collect();
+
+            for i in 0..9 {
+                let value = chars.get(1 + i * 4);
+
+                if value != None {
+                    let value = value.unwrap();
+                    if *value != ' ' {
+                        stacks_part_1[i].insert(0, *value);
+                    }
+                }
+            }
+        } else {
+            // Start applying the movement
+            let parts: Vec<&str> = line.split(' ').collect();
+            let amount: usize = parts[1].parse().unwrap();
+            let origin: usize = parts[3].parse().unwrap();
+            let destination: usize = parts[5].parse().unwrap();
+
+            // println!("move={} from={} to={} - {:?} - {}", amount, origin, destination, parts, line);
+
+            // Part 1
+            for _ in 0..amount {
+                let target = stacks_part_1[origin - 1].pop();
+                if target != None {
+                    stacks_part_1[destination - 1].push(target.unwrap());
+                }
+            }
+
+            // Part 2
+            {
+                let add_pos = stacks_part_2[destination - 1].len();
+                for _ in 0..amount {
+                    let target = stacks_part_2[origin - 1].pop();
+                    if target != None {
+                        stacks_part_2[destination - 1].insert(add_pos, target.unwrap());
+                    }
+                }
+            }
+        }
+    }
+
+    {
+        // Result for Part 1
+        let mut result_part_1 = vec![];
+        for i in 0..stacks_part_1.len() {
+            let last = stacks_part_1[i].last().unwrap();
+            if *last != ' ' {
+                result_part_1.push(*last);
+            }
+        }
+
+        println!("Result (Part 1): {} (expected: VCTFTJQCG)", result_part_1.into_iter().collect::<String>());
+    }
+
+    {
+        // Result for Part 2
+        let mut result_part_2 = vec![];
+        for i in 0..stacks_part_2.len() {
+            let last = stacks_part_2[i].last().unwrap();
+            if *last != ' ' {
+                result_part_2.push(*last);
+            }
+        }
+
+        println!("Result (Part 2): {} (expected: GCFGLDNJZ)", result_part_2.into_iter().collect::<String>());
+    }
+}
