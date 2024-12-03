52c0845d5e1c76634229af00732cf6aa0aa9562f
Andy Bitz <artzbitz@gmail.com>
2024-12-03 06:44:44 +0100

    Clean up the code a bit

--- 2024/day-03/main.go
+++ 2024/day-03/main.go
@@ -25,62 +25,49 @@ func partOne(mem string) {
 	var sum int64 = 0
 
 	for i := 0; i < len(mem); i++ {
-		if len(mem) > i+3 {
-			inst := mem[i : i+3]
-
-			// fmt.Println("Instruction", inst)
-
-			if inst != "mul" {
-				continue
-			}
-
-			if mem[i+3] != '(' {
-				continue
-			}
-
-			j := i + 4
-			lhs := ""
-			rhs := ""
-
-			current := &lhs
-
-			done := false
-
-			for {
-				next := mem[j]
-
-				if next == ')' {
-					done = true
-					break
-				}
-
-				if next == ',' {
-					current = &rhs
-					j += 1
-					continue
-				}
-
-				_, err := strconv.ParseInt(string(next), 0, 64)
-				if err != nil {
-					break
-				}
-
-				*current += string(next)
-				j += 1
-			}
-
-			if !done {
-				continue
-			}
-
-			left, _ := strconv.ParseInt(lhs, 0, 64)
-			right, _ := strconv.ParseInt(rhs, 0, 64)
-
-			// fmt.Println("Found", lhs, rhs)
-
-			sum += left * right
-			i = j
+		if len(mem) < i+4 || mem[i:i+4] != "mul(" {
+			continue
 		}
+
+		j := i + 4
+		done := false
+
+		lhs := ""
+		rhs := ""
+		current := &lhs
+
+		for {
+			next := mem[j]
+
+			if next == ')' {
+				done = true
+				break
+			}
+
+			if next == ',' {
+				current = &rhs
+				j += 1
+				continue
+			}
+
+			_, err := strconv.ParseInt(string(next), 0, 64)
+			if err != nil {
+				break
+			}
+
+			*current += string(next)
+			j += 1
+		}
+
+		if !done {
+			continue
+		}
+
+		left, _ := strconv.ParseInt(lhs, 0, 64)
+		right, _ := strconv.ParseInt(rhs, 0, 64)
+
+		sum += left * right
+		i = j
 	}
 
 	fmt.Println("Result:", sum)
@@ -91,73 +78,71 @@ func partTwo(mem string) {
 	enabled := true
 
 	for i := 0; i < len(mem); i++ {
-		if len(mem) > i+3 {
-			if len(mem) > i+4 && mem[i:i+4] == "do()" {
-				enabled = true
-				continue
-			}
-
-			if len(mem) > i+7 && mem[i:i+7] == "don't()" {
-				enabled = false
-				continue
-			}
-
-			if !enabled {
-				continue
-			}
-
-			mulInst := mem[i : i+3]
-			if mulInst != "mul" {
-				continue
-			}
-
-			if mem[i+3] != '(' {
-				continue
-			}
-
-			j := i + 4
-			lhs := ""
-			rhs := ""
-
-			current := &lhs
-
-			done := false
-
-			for {
-				next := mem[j]
-
-				if next == ')' {
-					done = true
-					break
-				}
-
-				if next == ',' {
-					current = &rhs
-					j += 1
-					continue
-				}
-
-				_, err := strconv.ParseInt(string(next), 0, 64)
-				if err != nil {
-					break
-				}
-
-				*current += string(next)
-				j += 1
-			}
-
-			if !done {
-				continue
-			}
-
-			left, _ := strconv.ParseInt(lhs, 0, 64)
-			right, _ := strconv.ParseInt(rhs, 0, 64)
-
-			// fmt.Println("Found", lhs, rhs)
-
-			sum += left * right
-			i = j
+		if len(mem) > i+4 && mem[i:i+4] == "do()" {
+			enabled = true
+			continue
 		}
+
+		if len(mem) > i+7 && mem[i:i+7] == "don't()" {
+			enabled = false
+			continue
+		}
+
+		if !enabled {
+			continue
+		}
+
+		if len(mem) > i+2 && mem[i:i+3] != "mul" {
+			continue
+		}
+
+		if len(mem) > i+3 && mem[i+3] != '(' {
+			continue
+		}
+
+		j := i + 4
+		done := false
+
+		lhs := ""
+		rhs := ""
+		current := &lhs
+
+		for {
+			if len(mem) < j {
+				break
+			}
+
+			next := mem[j]
+
+			if next == ')' {
+				done = true
+				break
+			}
+
+			if next == ',' {
+				current = &rhs
+				j += 1
+				continue
+			}
+
+			_, err := strconv.ParseInt(string(next), 0, 64)
+			if err != nil {
+				break
+			}
+
+			*current += string(next)
+			j += 1
+		}
+
+		if !done {
+			continue
+		}
+
+		left, _ := strconv.ParseInt(lhs, 0, 64)
+		right, _ := strconv.ParseInt(rhs, 0, 64)
+
+		sum += left * right
+		i = j
 	}
 
 	fmt.Println("Result:", sum)
